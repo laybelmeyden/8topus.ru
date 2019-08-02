1b18b9c8961ce5e0b66f15e5bcd8d97bb0a7df74
laybelmeyden <trycollens@gmail.com>
2019-08-02 22:00:45 +0300

    pusj

---
+++ resources/views/email/emai2.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Message from site</title>
+  </head>
+  <body>
+    Имя - {{ $name_text }} <br />
+    Почта - {{ $phone }} <br />
+    Сообщение - {{ $city }} <br />
+  </body>
+</html>
